197ecebc52fd5967f2ef799e9de290cd241c87d2
Eric Heidbreder <41808482+EricHeidbreder@users.noreply.github.com>
2023-04-19 14:40:23 -0500

    working etl app

--- micro-etl-app/app/app.py
+++ micro-etl-app/app/app.py
@@ -21,57 +21,48 @@ import logging
 import pandas as pd
 import os
 import requests
-import io
+import json
+import datetime
 
 # Environment variables
 URL = os.environ['Url']
 S3_BUCKET = os.environ['S3Bucket']
 LOG_LEVEL = os.environ['LogLevel']
-FILENAME = os.environ['Filename']
+FILENAME = os.environ['Filename'].split('.')[0] + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csv'
 
 # Log settings
 logger = logging.getLogger()
 logger.setLevel(LOG_LEVEL)
-
-# Function to support partial requests
-def range_header(start=0, end=0):    
-    if start < 0 and end == 0:
-        byte_range = 'bytes=%s' % (start)
-    else:        
-        byte_range = 'bytes=%s-%s' % (start, end)
-
-    return {'Range': byte_range}
 
 # Lambda function handler
 def lambda_handler(event, context):
     logger.info('## EVENT')
     logger.info(event)
     
-    columns = ['Date', 'Region_Name', 'Area_Code', 'Detached_Average_Price',
-       'Detached_Index', 'Detached_Monthly_Change', 'Detached_Annual_Change',
-       'Semi_Detached_Average_Price', 'Semi_Detached_Index',
-       'Semi_Detached_Monthly_Change', 'Semi_Detached_Annual_Change',
-       'Terraced_Average_Price', 'Terraced_Index', 'Terraced_Monthly_Change',
-       'Terraced_Annual_Change', 'Flat_Average_Price', 'Flat_Index',
-       'Flat_Monthly_Change', 'Flat_Annual_Change']
+    # columns = ['Date', 'Region_Name', 'Area_Code', 'Detached_Average_Price',
+    #    'Detached_Index', 'Detached_Monthly_Change', 'Detached_Annual_Change',
+    #    'Semi_Detached_Average_Price', 'Semi_Detached_Index',
+    #    'Semi_Detached_Monthly_Change', 'Semi_Detached_Annual_Change',
+    #    'Terraced_Average_Price', 'Terraced_Index', 'Terraced_Monthly_Change',
+    #    'Terraced_Annual_Change', 'Flat_Average_Price', 'Flat_Index',
+    #    'Flat_Monthly_Change', 'Flat_Annual_Change']
     
     # Request to get the last 2000000 bytes to get the most recent data in the CSV skipping the first row
     # Implies that the value are in ascending order with most recent at the end of the file
-    res = requests.get(URL, headers=range_header(-2000000), allow_redirects=True)
-    df = pd.read_csv(io.StringIO(res.content.decode('utf-8')), engine='python', error_bad_lines=False, names=columns, skiprows=1)
+    res = requests.get(URL)
+
+    if res.status_code == 200:
+        data = json.loads(res.text)
+    else:
+        logger.info(f'## ERROR | Status Code: {res.status_code}')
+    data_per_record = list(map(lambda x: {**x['geometry'], **x['properties']}, data['features'])) # merge the geometry and properties objects of each feature
+    morel_df = pd.DataFrame(data_per_record)
     logger.info('## NUMBER OF ELEMENTS')
-    logger.info(df.size)
-    
-    # Extract only values in a specified time range
-    start_date = '2018-01-01'
-    end_date = '2018-12-31'
-    date_range = (df['Date'] >= start_date) & (df['Date'] <= end_date)
-    df = df[date_range]
-    logger.info('## NUMBER OF ELEMENTS IN THE RANGE')
-    logger.info(df.size)
+    logger.info(morel_df.size)
 
     # Save files into S3
+    boto3.resource('s3').Bucket(S3_BUCKET).put_object(Key=FILENAME, Body=morel_df.to_csv(index=False))
     url = 's3://{}/{}'.format(S3_BUCKET, FILENAME)
-    df.to_csv(url)
+    # morel_df.to_csv(url)
     logger.info('## FILE PATH')
     logger.info(url)
